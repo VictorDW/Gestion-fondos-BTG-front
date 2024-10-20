f48e2e826f2603713a4c9c55ffc0af835f4f707c
victorADW <victor.agudelo@pragma.com.co>
2024-10-19 22:15:06 -0500

    feature: se creo el componente para las paginas y se configuro el routing

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -3,7 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { 
+    path: '', component: DashboardComponent,
+    children: [
+      {
+        path: 'funds', 
+        loadChildren: () => import('./pages/funds/funds.module').then( module => module.FundsModule)
+      },
+      {
+        path: '**',
+        redirectTo: 'funds',
+        pathMatch: 'full'
+      }
+    ]
+  }
 ];
 
 @NgModule({
