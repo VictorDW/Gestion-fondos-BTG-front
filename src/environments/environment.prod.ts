5d06a462289b1ce01306e451ab37b292af8f1589
victorADW <victor.agudelo@pragma.com.co>
2024-10-24 12:05:30 -0500

    feature: se creo un nuevo archivo de ambiente para prod y se configuro el angular.json, para que reemplaze el env por el env.prod cuando se despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
-  production: false,
-  BASE_URL: 'http://localhost:8080/api/'
+  production: true,
+  BASE_URL: 'http://3.235.95.70:8080/api/'
 }
 
 export const clientId = '670ff7facaba27fa00d268ae';
